0effdf247c1fcb695674aefe89f9ccd63a3a3e96
walterfall <orz0916840932@hotmail.com>
2017-03-25 00:30:06 +0800

    init

---
+++ Northwind.EF.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace northwind.ef.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
